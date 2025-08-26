1abc98b2ebcca7c4bcfb55c4a57448dcbd0ce808
aslihan-inan <a.uzunogullari@gmail.com>
2025-08-26 14:36:07 +0300

    güncellemeler

--- src/layout/mainlayout.jsx
+++ src/layout/mainlayout.jsx
@@ -3,13 +3,12 @@ import React from "react";
 import Header from "./Header";
 import Footer from "../Footer";
 
-const MainLayout = ({ children }) => {
+export default function MainLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
+    <div>
+     <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   );
-};
-export default MainLayout;
+}
