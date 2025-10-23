ba66d7f874706104c30ff61c686686d9615c2c9e
aslihan-inan <a.uzunogullari@gmail.com>
2025-10-23 15:23:07 +0300

    Tüm dosyalar güncellendi

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// server.js veya app.js
 import express from "express";
 import cors from "cors";
 
@@ -24,7 +23,7 @@ import userRoutes from "./routes/userRoutes.js";
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/user", userRoutes);
+
 
 // Server başlat
 const PORT = process.env.PORT || 5000;
