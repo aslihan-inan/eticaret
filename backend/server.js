c290d00ea1f58e9eb6defb1a58434957f964716f
aslihan-inan <a.uzunogullari@gmail.com>
2025-10-23 12:15:04 +0300

    Tüm dosyalar güncellendi

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://eticaret-26-git-main-aslihan-inans-projects.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://eticaret-nine.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
